53dde3d099f064a156d6017a5dcfa09b16d2c854
Guilherme Gomes <guilhermeneto26@hotmail.com>
2025-06-12 17:30:56 -0300

    Add byscarEmLargura method

--- src/lib/Grafo.java
+++ src/lib/Grafo.java
@@ -3,7 +3,7 @@ package lib;
 import java.util.ArrayList;
 
 public class Grafo<T> {
-    private ArrayList<Aresta> arestas;
+    private ArrayList<Aresta<T>> arestas;
     private ArrayList<Vertice<T>> vertices;
 
     public Vertice<T> adicionarVertice(T valor){
@@ -21,6 +21,17 @@ public class Grafo<T> {
         }
         //Se chegou até aqui é poruqe não existe um vertice com esse valor
         return null;
+    }
+
+    private ArrayList<Aresta<T>> obterDestinos(Vertice<T> v){
+        ArrayList<Aresta<T>> destinos = new ArrayList<Aresta<T>>();
+        Aresta<T> atual;
+        for (Aresta<T> aresta : this.arestas) {
+            atual = aresta;
+            if (atual.getOrigem().equals(v))
+                destinos.add(atual);
+        }
+        return destinos;
     }
 
     public void adicionarAresta(T origem, T destino, float peso){
@@ -41,6 +52,26 @@ public class Grafo<T> {
         //Adicionei a aresta a lista do Grafo
         this.arestas.add(novaAresta);
 
+    }
+
+    public void buscarEmLargura(){
+        ArrayList<Vertice<T>> marcados = new ArrayList<Vertice<T>>();
+        ArrayList<Vertice<T>> fila = new ArrayList<Vertice<T>>();
+        Vertice<T> atual=this.vertices.get(0);
+        fila.add(atual);
+        while(fila.size()>0){
+            atual = fila.get(0);
+            fila.remove(0);
+            marcados.add(atual);
+            System.out.println(atual.getValor());
+            ArrayList<Aresta<T>> destinos = this.obterDestinos(atual);
+            Vertice<T> proximo;
+            for (Aresta<T> destino : destinos) {
+                proximo = destino.getDestino();
+                if (!marcados.contains(proximo) && !fila.contains(proximo))
+                    fila.add(proximo);
+            }
+        }
     }
 
 }
