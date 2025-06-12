bcca1ee1933302492c5e0b9fc0a62a3e6007fb09
Guilherme Gomes <guilhermeneto26@hotmail.com>
2025-06-12 11:50:37 -0300

    Inntitial classes for graph

---
+++ src/lib/Aresta.java
@@ -0,0 +1,6 @@
+package lib;
+
+public class Aresta {
+    private Vertice origem, destino;
+    private float peso;
+}
---
+++ src/lib/Grafo.java
@@ -0,0 +1,8 @@
+package lib;
+
+import java.util.ArrayList;
+
+public class Grafo<T> {
+    private ArrayList<Aresta> arestas;
+    private ArrayList<Vertice<T>> vertices;
+}
