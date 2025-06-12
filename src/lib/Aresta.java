91bedfb1bc7e2c7f134264c1efe6ecb59df67623
Guilherme Gomes <guilhermeneto26@hotmail.com>
2025-06-12 16:18:28 -0300

    Create constructor and add Generics

--- src/lib/Aresta.java
+++ src/lib/Aresta.java
@@ -1,6 +1,12 @@
 package lib;
 
-public class Aresta {
-    private Vertice origem, destino;
+public class Aresta<T> {
+    private Vertice<T> origem, destino;
     private float peso;
+
+    public Aresta(Vertice<T> origem, Vertice<T> destino, float peso) {
+        this.origem = origem;
+        this.destino = destino;
+        this.peso = peso;
+    }
 }
