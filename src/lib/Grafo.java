a1bae4e5573fb0e0ebb3df361effbb59c080186f
Guilherme Gomes <guilhermeneto26@hotmail.com>
2025-06-12 16:16:58 -0300

    Create adicionarVertice method

--- src/lib/Grafo.java
+++ src/lib/Grafo.java
@@ -5,4 +5,11 @@ import java.util.ArrayList;
 public class Grafo<T> {
     private ArrayList<Aresta> arestas;
     private ArrayList<Vertice<T>> vertices;
+
+    public Vertice<T> adicionarVertice(T valor){
+        Vertice<T> novo = new Vertice<T>(valor);
+        this.vertices.add(novo);
+        return novo;
+    }
+
 }
